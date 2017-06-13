00d098b21cad8968e3ea3c91a3d0221903bc72a4
4iar <me@4iar.com>
2017-06-13 22:54:25 +0100

    Add API scaffolding

--- include/nicehash-api.hpp
+++ include/nicehash-api.hpp
@@ -1,7 +1,24 @@
+#include <string>
 
 
 class NiceHashApi {
 public:
-    int x;
-};
+    // Global
+    std::string getCurrentGlobalStats ();
+    std::string getAverageGlobalStats ();
 
+    // Provider
+    std::string getProviderStats ();
+    std::string getDetailedProviderStats ();
+    std::string getProviderWorkers ();
+
+    // Orders
+    std::string getAlgorithmOrders ();
+    std::string getMultiAlgorithmInfo ();
+    std::string getSimpleMultiAlgorithmInfo ();
+
+    // Helpers
+    int algorithmNameToId (std::string algorithm_name);
+    std::string algorithmIdToName (int algorithm_id);
+    std::string getApiVersion ();
+};
